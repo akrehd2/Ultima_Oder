381747e7acb33fbfe1066cb6dc621c42ba458b51
winder24962 <106226709+winder24962@users.noreply.github.com>
2024-01-10 11:01:35 +0100

    cam_comp

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject target;
+    public float moveSpeed;
+    private Vector3 targetPosition;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target.gameObject != null)
+        {
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+}
